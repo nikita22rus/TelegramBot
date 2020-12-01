ecc19c20fc69bfc0c4d734ece5b1226bc6181981
nikita_tililitsin <nikita_tililicin@mail.ru>
2020-12-01 11:01:46 +0700

    first

--- Bot.py
+++ Bot.py
@@ -1,41 +1,92 @@
-from flask import Flask, request, make_response, jsonify
+import telebot
+import config
+import random
+
 import requests
 import json
-from geopy.geocoders import Nominatim
+import time
 
-app = Flask(__name__)
+import config
 
-API_KEY = '<your_AP_KEY_here>'
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+def answer_user_bot(data):
+    data = {
+        'chat_id': config.MY_ID,
+        'text': data
+    }
+    url = config.URL.format(
+        token=config.TOKEN,
+        method=config.SEND_METH
+    )
+    response = requests.post(url, data=data)
 
-def results():
-    req = request.get_json(force=True)
 
-    action = req.get('queryResult').get('action')
+def parse_weather_data(data):
+    for elem in data['weather']:
+        weather_state = elem['main']
+    temp = round(data['main']['temp'] - 273.15, 2)
+    city = data['name']
+    msg = f'The weather in {city}: Temp is {temp}, State is {weather_state}'
+    return msg
 
-    result = req.get("queryResult")
-    parameters = result.get("parameters")
 
-    if parameters.get('location').get('city'):
-        geolocator = Nominatim(user_agent='weather-bot')
-        location = geolocator.geocode(parameters.get('location').get('city'))
-        lat = location.latitude
-        long = location.longitude
-        weather_req = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}'.format(lat, long, API_KEY))
-        current_weather = json.loads(weather_req.text)['current']
-        temp = round(current_weather['temp'] - 273.15)
-        feels_like = round(current_weather['feels_like'] - 273.15)
-        clouds = current_weather['clouds']
-        wind_speed = current_weather['wind_speed']
+def get_weather(location):
+    url = config.WEATHER_URL.format(city=location,
+                                   token=config.WEATHER_TOKEN)
+    response = requests.get(url)
+    if response.status_code != 200:
+        return 'city not found'
+    data = json.loads(response.content)
+    return parse_weather_data(data)
 
-    return {'fulfillmentText': 'Сейчас температура воздуха - {} градусов, ощущается как {} градусов, облачность - {}%, скорость ветра - {}м/с'.format(str(temp), str(feels_like), str(clouds), str(wind_speed))}
 
-@app.route('/webhook', methods=['GET', 'POST'])
-def webhook():
-    return make_response(jsonify(results()))
+def get_message(data):
+    return data['message']['text']
+
+
+def save_update_id(update):
+    with open(config.UPDATE_ID_FILE_PATH, 'w') as file:
+        file.write(str(update['update_id']))
+    config.UPDATE_ID = update['update_id']
+    return True
+
+
+def main():
+    while True:
+        url = config.URL.format(token=config.TOKEN, method=config.UPDATE_METH)
+        content = requests.get(url).text
+
+        data = json.loads(content)
+        result = data['result'][::-1]
+        needed_part = None
+
+        for elem in result:
+            if elem['message']['chat']['id'] == const.MY_ID:
+                needed_part = elem
+                break
+
+        if config.UPDATE_ID != needed_part['update_id']:
+            message = get_message(needed_part)
+            msg = get_weather(message)
+            answer_user_bot(msg)
+            save_update_id(needed_part)
+
+        time.sleep(1)
+
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    main()
+
+
+
+#bot = telebot.TeleBot(config.TOKEN)
+
+#@bot.message_handler(content_types=['text'])
+#def lalala(message):
+    #bot.send_message(message.chat.id, message.text)
+
+
+
+# RUN
+#bot.polling(none_stop=True)
+
